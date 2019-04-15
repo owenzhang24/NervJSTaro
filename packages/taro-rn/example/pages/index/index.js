1c13676629722476eb545369a25c65d8a93fb4f5
Pines-Cheng <spider.cs.nuc@gmail.com>
2019-04-15 19:36:41 +0800

    feat: add example in taro-rn

--- packages/taro-rn/example/pages/index/index.js
+++ packages/taro-rn/example/pages/index/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Taro, { Component } from '@tarojs/taro-rn'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
+
+const {JDLocation} = require('@jdreact/jdreact-core-lib')
 
 const styles = StyleSheet.create({
   index: {
@@ -20,10 +22,20 @@ export default class Index extends Component {
 
   componentDidHide () { }
 
+  getLocation () {
+    console.log('getLocation')
+    JDLocation.getCurrentPosition((result) => {
+      console.log('getCurrentPosition success:' + JSON.stringify(result))
+    }, (code, reason) => {
+      console.log('getCurrentPosition error, code' + code + ', reason:' + reason)
+    }, null)
+  }
+
   render () {
     return (
       <View style={{paddingTop: 20}}>
         <Text style={styles.index}>Hello world!</Text>
+        <Button onPress={this.getLocation.bind(this)} title='getLocation' />
       </View>
     )
   }
